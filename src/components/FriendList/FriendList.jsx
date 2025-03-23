d4842569bf5abcec3281e20e38d8177e5960737a
Ivan_Pavlyshyn <ivanpavlushun2000@gmail.com>
2025-03-23 12:03:52 +0100

    goit-hw-react ver:3

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import s from "./FriendList.module.css";
 import clsx from "clsx";
+import FriendListItem from "./FriendListItem";
 
 export const FriendList = ({ friends }) => {
   return (
     <ul className={s.friend_list}>
-      {friends.map((friend) => (
-        <li key={friend.id} className={s.box}>
-          <img src={friend.avatar} alt="Avatar" width="64" />
-          <p>{friend.name}</p>
-          <p className={clsx(s.status, friend.isOnline ? s.green : s.red)}>
-            {friend.isOnline ? "Online" : "Offline"}
-          </p>
-        </li>
+      {friends.map(({id, name, avatar, isOnline}) => (
+        <FriendListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
       ))}
     </ul>
   );
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import s from "./FriendList.module.css";
+import clsx from "clsx";
+
+export const FriendListItem = ({avatar, name, isOnline}) => {
+  return (
+    <li className={s.box}>
+      <img src={avatar} alt="Avatar" width="64" />
+      <p>{name}</p>
+      <p className={clsx(s.status, isOnline ? s.green : s.red)}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </li>
+  );
+}
+
+export default FriendListItem;
